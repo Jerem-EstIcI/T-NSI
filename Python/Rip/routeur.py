1e2622fb018d7c8a8c451e757e87e41cefb7e60c
jerem <114385056+Jerem-EstIcI@users.noreply.github.com>
2023-12-19 12:09:31 +0100

    Update routeur.py

--- Python/Rip/routeur.py
+++ Python/Rip/routeur.py
@@ -1,28 +1,22 @@
 class Routeur():
   def __init__(self, nom):
     # à compléter
-
+    self.nom=nom
+    self.table=dict() # ou {}
 
   def maj_table(self, destination,passerelle,saut):
-    # à compléter
+    self.table[destination]=[passerelle,saut]
 
-
-  def recoit(self, routeur):
-    # à compléter
+  def recoit(self, routeurVoisin):
     #on parcourt la table du routeur
-
-        #ce n'est pas un lien vers ce routeur
-
-
-            if
-                # le routeur n'est pas dans la table : on l'ajoute
-
-
-            else :
-                # le routeur est déja dans la table : on met à jour la table
-
-
-
+    for cle in routeurVoisin.table.keys():
+      if cle!=self.nom: #ce n'est pas un lien vers ce routeur   
+        if not cle in self.table.keys(): # le routeur n'est pas dans la table : on l'ajoute
+          self.maj_table(cle,routeurVoisin.nom,routeurVoisin.table(cle)[1]+1)
+        else: # le routeur est déja dans la table : on met à jour la table
+          AncienSaut=self.table[cle][1]
+          if routeurVoisin.table(cle)[1]+1<AncienSaut:
+            self.maj_table(cle,routeurVoisin.nom,routeurVoisin.table(cle)[1]+1)
 
 
   def __str__(self):
